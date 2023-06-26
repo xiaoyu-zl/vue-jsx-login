002ee605184e652b369d186b66da4925d51aebb7
zzl <cherish667zl@163.com>
2023-06-26 17:28:25 +0800

    删除多余代码

--- src/views/user/index.tsx
+++ src/views/user/index.tsx
@@ -1,10 +1,8 @@
-import { defineComponent, ref, reactive } from "vue";
+import { defineComponent, ref } from "vue";
 import { useRouter } from "vue-router";
 import register from "./register.tsx";
 import login from "./login.tsx";
-
 import style from "@/moduleScss/login.module.scss"; //以.module做样式隔离
-import type { FormInstance, FormRules } from "element-plus";
 export default defineComponent({
   components: { register, login },
   setup() {
@@ -16,39 +14,7 @@ export default defineComponent({
       return new URL(url, import.meta.url).href;
     };
     //form
-    type FormData = {
-      userName: string;
-      password: string;
-    };
-    const formData = ref<FormData>({ userName: "", password: "" });
-    const rules = reactive<FormRules>({
-      userName: [
-        {
-          required: true,
-          message: "请输入用户名",
-          trigger: "change",
-        },
-      ],
-      password: [
-        {
-          required: true,
-          message: "请输入用户名密码",
-          trigger: "change",
-        },
-        {
-          min: 6,
-          max: 18,
-          message: "长度应为 6 ~ 18",
-          trigger: "change",
-        },
-      ],
-    });
     const off = ref(true);
-    const formRef = ref<FormInstance>();
-    const submitForm = async () => {
-      const isOk = await formRef.value.validate((valid: boolean) => valid);
-      console.log(isOk);
-    };
     const handoff = () => {
       off.value = !off.value;
     };
