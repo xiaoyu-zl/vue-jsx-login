3dbe588b34ec17456b4c3b8b87ff25e3782aa2f9
zzl <cherish667zl@163.com>
2023-08-30 11:36:27 +0800

    bus改造

--- src/views/user/bus.js
+++ src/views/user/bus.js
@@ -1,3 +1,5 @@
+const { el } = require("element-plus/es/locale");
+
 //ljp
 const eventBus = function () {
   const eventMap = new Map();
@@ -10,18 +12,16 @@ const eventBus = function () {
           const getMapBacks = eventMap.get(eventName);
           getMapBacks.push(callBack);
         }
-        eventMap.get("*").push(callBack);
+        eventMap.get("*").push(callBack); //存入所有订阅事件
       } else {
         eventMap.set(eventName, [callBack]);
+        eventMap.set("*", [...eventMap.get("*"), callBack]); //存入所有订阅事件
       }
     },
     emit(eventName, eventValue) {
       const isCallBack = eventMap.has(eventName);
       if (isCallBack) {
         for (const callBack of eventMap.get(eventName)) {
-          if (eventName != "*") callBack(eventValue);
-        }
-        for (const callBack of eventMap.get("*")) {
           callBack(eventValue);
         }
       }
@@ -40,41 +40,66 @@ const eventBus = function () {
 };
 const bus = new eventBus();
 bus.on("bus", (e) => {
-  console.log("触发bus，我监听了", e);
+  console.log("触发bus1，我监听了", e);
 });
-bus.emit("bus", "ddd");
+bus.on("bus", (e) => {
+  console.log("触发bus2，我监听了", e);
+});
+bus.on("sss", (e) => {
+  console.log("触发sss，我监听了", e);
+});
+bus.emit("sss", "ddd");
+bus.emit("*", "ddd");
 
 //zzl
 function EvBus() {
   let eventObject = {};
   return {
-    on(eventName, callback) {
+    on(eventName, callback, key = "*") {
       if (!eventObject[eventName]) {
         eventObject[eventName] = [];
       }
-      eventObject[eventName].push(callback);
+      eventObject[eventName].push({ key, callback });
     },
-    off(eventName) {
-      if (eventObject[eventName]) {
+    off(eventName, key = "*") {
+      if (eventObject[eventName] && key == "*") {
         delete eventObject[eventName];
+      } else {
+        eventObject[eventName] = eventObject[eventName].filter(
+          (v) => v.key != key
+        );
       }
-      return console.log(eventName + "注销成功");
     },
-    emit(eventName, ...args) {
+    emit(eventName, args, key = "*") {
       const callbackList = eventObject[eventName];
       if (!callbackList) return console.error(eventName + "没有这个事件");
-      for (let callback of callbackList) {
-        callback(...args);
+      for (let item of callbackList) {
+        if (key != "*" && item.key == key) {
+          item.callback(args);
+          return;
+        }
+
+        if (key == "*") {
+          item.callback(args);
+        }
       }
     },
   };
 }
 const eBus = new EvBus();
-eBus.on("X", (obj, num) => {
-  console.log("A事件", obj, num);
-});
-eBus.on("X", (obj, num) => {
-  console.log("B事件", obj, num);
-});
+eBus.on(
+  "X",
+  (obj) => {
+    console.log("A事件", obj);
+  },
+  "A事件"
+);
+eBus.on(
+  "X",
+  (obj) => {
+    console.log("B事件", obj);
+  },
+  "B事件"
+);
 // eBus.off("X");
-eBus.emit("X", { name: "X订阅" }, 1);
+eBus.emit("X", { name: "X订阅" }, "B事件");
